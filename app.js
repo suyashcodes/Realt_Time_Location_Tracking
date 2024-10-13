f23b712b23226794b9152605f4ac3cc7b6373f9b
Suyash Dashputre <suyashdashputre@gmail.com>
2024-10-13 11:15:56 +0530

    Project Created

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.get("/",function(req,res){
+    res.send("hey");
+});
+
+server.listen(3000);
